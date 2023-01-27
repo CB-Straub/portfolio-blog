c34e320741f84bf00ea1cd18911483566713d535
Brian Straub <cbstraub@protonmail.com>
2023-01-27 16:01:17 -0700

    added directory change

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const router = jsonServer.router('db.json');
 const port = process.env.PORT || 3001;
 
 app.use('/', middlewares, router);
-app.use(express.static(path.join(__dirname,  'build')));
+app.use(express.static(path.join(__dirname,  '..', 'build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname,'build', 'index.html'));
+    res.sendFile(path.join(__dirname,'..', 'build', 'index.html'));
 });
 
 server.listen(port);
