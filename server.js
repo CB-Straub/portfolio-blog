f6370939fe580f2e45e1eac2785390301b61cee5
Brian Straub <cbstraub@protonmail.com>
2023-01-27 16:35:27 -0700

    app.get changed, added /

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 app.use('/db', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
