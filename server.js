98a7e840c58ab2e75d0d0ee9c81491261a953274
Brian Straub <cbstraub@protonmail.com>
2023-01-27 15:54:32 -0700

    server.js removed / from app.get

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 app.use('/db', middlewares, router);
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'frontend','build', 'index.html'));
 });
 
