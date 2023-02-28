c6296a584b3f8892a4ba2c7c25313f618bb11508
Brian Straub <cbstraub@protonmail.com>
2023-02-27 18:02:22 -0700

    title changes

--- src/components/NavImage.js
+++ src/components/NavImage.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const NavImage = () => {
   return (
+    <div>
     <div className='navImage'> 
-      <div> Blogdiggity Music Blog </div>
+      <div> BlogDoggle </div>
+      <p>    :a blog on music</p>
     </div>
+   
+    </div>
+   
   )
 }
 
